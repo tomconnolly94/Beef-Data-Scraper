7bbab8a10b89a3f72d45eb108640343df30197d8
tom <tom.connolly2511@gmail.com>
2017-09-11 22:30:55 +0100

    bug fixes

--- scrapers/cnn_scraper/sub_page_scrapers/cnn_article_scraper.py
+++ scrapers/cnn_scraper/sub_page_scrapers/cnn_article_scraper.py
@@ -56,16 +56,22 @@ def scrape_article(path, uReq, soup, keyword_list):
             if "technology" in path:
                 categories.append(6)
             
-            img_tag_array = sub_page_soup.findAll("div", {"class" : "el__image--fullwidth"}) #find tags in the soup object
+            media_tag_array = sub_page_soup.findAll("div", {"class" : "el__image--fullwidth"}) #find tags in the soup object
             
             img_link = ""
             
-            if len(img_tag_array) > 0: #if article contains references to images, extract the first one
-                print(img_tag_array)
-                img_link = img_tag_array[0].div.img['data-src-large']
+            if len(media_tag_array) > 0 and media_tag_array[0].div.img['data-src-large']: #if article contains references to images, extract the first one
+                img_link = media_tag_array[0].div.img['data-src-large']
             
-                
+            media_link = ""
+            '''
+            if len(media_tag_array) > 0 and media_tag_array[0].div.video: #if article contains references to images, extract the first one
+                script_text = tag_array[10].text
+                result = re.search('CNN.contentModel = (.*);', script_text)
+                script_json = demjson.decode(result.group(1))
+                media_link = media_tag_array[0].div.video['data-src-large']
+            '''
             #frame BeefObject( title, relevant_actors, content, date, highlights, data_source, categories, img_title)
-            beef_obj = BeefObject(title_tag_array[0].text, actors_list, content_string, date_string, highlights, "path", categories, img_link) #create beefObject 
+            beef_obj = BeefObject(title_tag_array[0].text, actors_list, content_string, date_string, highlights, "path", categories, img_link, media_link) #create beefObject 
             
             return beef_obj
