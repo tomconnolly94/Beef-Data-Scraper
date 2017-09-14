df61100ee83a3907c0ba2de3e14e4a9f1d754a23
tom <tom.connolly2511@gmail.com>
2017-09-14 20:25:46 +0100

    insert error handling created

--- interfaces/insert_into_db.py
+++ interfaces/insert_into_db.py
@@ -27,6 +27,7 @@ def insert_if_not_exist(beef_object):
                 "data_sources" : beef_object.data_source,
                 "selected_categories" : beef_object.categories,
                 "img_title" : beef_object.img_title,
+                "media_link" : beef_object.media_link,
             })
 
             try:
@@ -37,3 +38,19 @@ def insert_if_not_exist(beef_object):
             except pymongo.errors.AutoReconnect:
                 print("Pymongo error, retrying db connection...")
                 db.close()
+        
+'''      
+beef_object = BeefObject(
+    "Storm Harvey: Trump to make second visit to Texas", 
+    ['Donald Trump', 'Trump', 'Louisiana', 'Abbott', 'Melania', 'Mrs Trump', 'Houston', 'Lake Charles', 'Mick Mulvaney', 'Mulvaney', 'Harvey', 'Texas', 'Major League Baseball', 'Reid Ryan'],
+    "We have already learned how to insert records into a collection and also learned how to update the document within a collection based on certain criteria. In this page, we are going to discuss how to remove document and entire collection.remove() is used to remove a document and a collection. It may be a good practice if you execute a find() before removing a document as you can can fix a criteria for removing the document.",
+    "2 September 2017", 
+    ['National Day of Prayer', "This request is a down-payment on the president's commitment to help affected states recover from the storm, and future requests will address longer-term rebuilding needs,", 'expeditiously to ensure that the debt ceiling does not affect these critical response and recovery efforts', 'multi-year project', 'This is going to be a massive, massive clean-up process,', 'poses an ongoing threat', 'We hope that these games can serve as a welcome distraction for our city that is going through a very difficult time,', 'We hope that we can put smiles on some faces.'], 
+    "http://www.bbc.co.uk/news/world-us-canada-41134799", 
+    [], 
+    "https://ichef-1.bbci.co.uk/news/320/cpsprodpb/32EA/production/_97643031_mediaitem97643030.jpg",
+    ""
+    )
+
+insert(beef_object)
+'''
