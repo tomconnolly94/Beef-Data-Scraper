be269cb8d8d96475efd7409fb39378a9bb811ae3
tom <tom.connolly2511@gmail.com>
2017-09-20 23:16:45 +0100

    actor_scraper dev

--- actor_scraping/actor_scraper_controller.py
+++ actor_scraping/actor_scraper_controller.py
@@ -6,19 +6,22 @@ from bs4 import BeautifulSoup as soup
 from wikipedia_scraper import pre_scrape_page_check
 from wikipedia_scraper import scrape_actor_from_wiki
 
-pre_scrape_check_result = pre_scrape_page_check(uReq, soup, sys.argv[1])
 
-if isinstance(pre_scrape_check_result, list):
-    
-    print("1")
-    print(pre_scrape_check_result)
+def scrape_actor(name):
+    pre_scrape_check_result = pre_scrape_page_check(uReq, soup, name)
 
-elif isinstance(pre_scrape_check_result, str):
-    
-    print("2")
-    print(scrape_actor_from_wiki(uReq, soup, pre_scrape_check_result))
+    if isinstance(pre_scrape_check_result, list):
 
-else:
-    
-    print("3")
-    print("pre-scrape check returned un-recognised value")
+        print("1")
+        print(pre_scrape_check_result)
+
+    elif isinstance(pre_scrape_check_result, str):
+
+        print("2")
+        print(scrape_actor_from_wiki(uReq, soup, pre_scrape_check_result))
+
+    else:
+
+        print("3")
+        print("pre-scrape check returned un-recognised value")
+        
---
+++ scrape_actor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#imports
+import sys
+from actor_scraping.actor_scraper_controller import scrape_actor
+
+scrape_actor(sys.argv[1])
