fbf6611af01c930b641e199e66605910b1cdd896
tom <tom.connolly2511@gmail.com>
2017-10-31 19:00:27 +0000

    he

--- scrape_actor.py
+++ scrape_actor.py
@@ -4,8 +4,6 @@ import sys
 import json
 from actor_scraping.actor_scraper_controller import scrape_actor
 
-print("scrape_actor.py started.")
-
 result = scrape_actor(sys.argv[1])
 
 print(json.dumps(result))
