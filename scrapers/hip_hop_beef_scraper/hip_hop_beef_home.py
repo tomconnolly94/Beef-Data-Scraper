f6b541cf7ea2cc72a00e501393346d50909f35e3
tom <tom.connolly2511@gmail.com>
2017-11-22 23:25:59 +0000

    big fixing

--- scrapers/hip_hop_beef_scraper/hip_hop_beef_home.py
+++ scrapers/hip_hop_beef_scraper/hip_hop_beef_home.py
@@ -42,24 +42,38 @@ def scrape_hip_hop_beef_home(uReq, soup, keyword_list):
                 a = tag.find("a")
 
                 if a and a["href"]:
-                    beef_object = scrape_article(a["href"], uReq, soup, keyword_list)
+                    
+                    sub_page_url = a["href"]
 
-                    if beef_object != None:
-                        beef_objects.append(beef_object)
+                    if any(url_obj["url"] == sub_page_url for url_obj in saved_urls): #check through pre loaded urls to ensure url has not already been scraped
+                        if logging:
+                            print("preloaded url found, aborting scrape.")
+
+                    else:
+                        if logging:
+                            print("preloaded url not found, initiating scrape.")
+
+                        #url must be saved under these conditions: 1. it has not been previously scraped, 2. it may not be relevant to beef and therefore may not be added to selected events, 
+                        save_url(base_url, sub_page_url)
+
+                        beef_object = scrape_article(sub_page_url, uReq, soup, keyword_list)
+
+                        if beef_object != None:
+                            beef_objects.append(beef_object)
 
         video_tag_array = page_soup.find("div", {"class", "items_list"})#, text=pattern) #find tags in the soup object
         video_tag_array = video_tag_array.findAll("li")#, text=pattern) #find tags in the soup object
 
         if len(video_tag_array) > 0: #only execute if tags have been found
 
-            percent_per_scrape = 100/len(news_tag_array)
+            percent_per_scrape = 100/len(video_tag_array)
             
             for x, tag in enumerate(video_tag_array):
                 
                 print(str(round(x * percent_per_scrape)) + "% complete.")
                 
                 a = tag.find("a")
-
+                
                 if a and a["href"]:
                     
                     sub_page_url = a["href"]
@@ -79,8 +93,7 @@ def scrape_hip_hop_beef_home(uReq, soup, keyword_list):
 
                         if beef_object != None:
                             beef_objects.append(beef_object)
-                            break;
-        
+                        
         return beef_objects
     else:
         return []    
