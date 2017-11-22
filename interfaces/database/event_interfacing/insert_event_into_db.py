7cbc77dcffb8e1f0d6148890a4d5ff5ad2806e2c
tom <tom.connolly2511@gmail.com>
2017-11-22 20:30:54 +0000

    bug fixes

--- interfaces/database/event_interfacing/insert_event_into_db.py
+++ interfaces/database/event_interfacing/insert_event_into_db.py
@@ -65,11 +65,11 @@ def inspect_beef_event_for_broken_fields(beef_object):
                     record_broken_field("img_link", beef_object_fields[5], "empty", field)
             
             #check field values for incorrect formatting or too long/short values
-            if index == 1 and len(field) < 2:
+            elif index == 1 and len(field) < 2:
                 record_broken_field("relevant_actors", beef_object_fields[5], "too_short", field)
             elif index == 2:
                 if len(field) < 100:
-                    record_broken_field("relevant_actors", beef_object_fields[5], "too_long", field)
+                    record_broken_field("relevant_actors", beef_object_fields[5], "too_short", field)
                 elif len(field) > 1000:
                     record_broken_field("relevant_actors", beef_object_fields[5], "too_long", field)
             elif index == 3:
